b9ce6e285327e2991c32b8f5767298260a42a172
awskt <sk_pro@outlook.com>
2022-05-26 23:04:59 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
 ﻿namespace SdamgiaParser;
 
-internal class Program {
+internal static class Program {
     const string Subject = "math-ege";
     const string BaseUrl = $"https://{Subject}.sdamgia.ru/get_solution?id=";
     const int Threads = 100;
 
-    static async Task Main(string[ ] args) {
+    static void Main(string[ ] _) {
 
         // Будет создана только если не существует
         Directory.CreateDirectory("Photos");
 
         // "Photos\158796274.WNhgfhg.jpg"
-        var lastidt = Directory.GetFiles("Photos").FirstOrDefault( )[7..].Split('.')[0];
-        var lastid = int.Parse(lastidt) - 1;
+        int lastid = 158796274;
+        try {
+            var lastidt = Directory.GetFiles("Photos").FirstOrDefault( )![7..].Split('.')[0];
+            lastid = int.Parse(lastidt) - 1;
+        } catch (Exception) { /* No photos error */ }
+
         object _lock = new( );
 
         CancellationTokenSource cancelTokenSource = new( );
@@ -37,19 +41,25 @@ internal class Program {
                 Console.WriteLine("Thread " + threadid + " started");
                 HttpClient client = new( );
                 while (!token.IsCancellationRequested) {
+                    try {
+
                     int i = GetNextId( );
-                    Console.Write($"\n[{threadid}] Get {i}");
                     var a = (await client.GetAsync(BaseUrl + i)).Content;
 
                     // Если файла не существует
                     if (a.Headers.ContentDisposition is null) {
                         continue;
                     }
-                    string filename = a.Headers.ContentDisposition.FileName[1..^1];
+                    string filename = a.Headers.ContentDisposition.FileName![1..^1];
+                    Console.Write($"\n[{threadid}] Get {i}");
                     Console.Write(" >> " + filename);
 
                     using var fs = new FileStream(Path.Combine("Photos", $"{i}.{filename}"), FileMode.CreateNew);
                     await a.CopyToAsync(fs);
+
+                    } catch (Exception ex) {
+                        Console.WriteLine("ERROR: " + ex.ToString( ));
+                    }
                 }
             });
         }
@@ -57,11 +67,11 @@ internal class Program {
     }
     public static int ThreadId {
         get {
-            return _threadId++;
+            return ThreadIdTemp++;
         }
         set {
-            _threadId = value;
+            ThreadIdTemp = value;
         }
     }
-    private static int _threadId { get; set; }
+    private static int ThreadIdTemp { get; set; }
 }
